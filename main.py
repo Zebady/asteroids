c499ea28d1bace8caffea7d53097db9f3bb0e4aa
Zebady <burgess_kris@hotmail.com>
2025-11-13 22:05:44 +0000

    Added Player Class

--- main.py
+++ main.py
@@ -2,22 +2,35 @@ import pygame
 
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
